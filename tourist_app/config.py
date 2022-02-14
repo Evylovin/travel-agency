519ef1829e014f914221508884a5f1efd979a8a2
Кириченко Кирилл <k.kirichenko@int.robotbull.com>
2022-02-14 12:23:37 +0300

    fix database

--- tourist_app/config.py
+++ tourist_app/config.py
@@ -1,12 +1,7 @@
-class Config:
-    # Данные конфигурации подключения к БД
-    dialect = 'postgresql'
-    username = 'postgres'
-    password = 'Qwerty7'
-    host = 'localhost'
-    db_name = 'tourist_agency'
+import os
 
+class Config:
     # Настройки для экземляра: секретный ключ запуска и путь к БД
-    SQLALCHEMY_DATABASE_URI = f'{dialect}://{username}:{password}@{host}/{db_name}'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'e12kome12modoi1290ems'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
